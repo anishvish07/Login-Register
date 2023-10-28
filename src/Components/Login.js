1f5fb277f4d433449416557f83dd54826cf7442e
anishvish07 <dumbodare12@gmail.com>
2023-10-28 21:08:03 +0530

    first commit

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,28 +1,35 @@
 import React from 'react';
+import './Register.css';
+import 'animate.css';
+
 import { BrowserRouter as Router, Switch , Link ,Route } from 'react-router-dom';
 const Login = (props) => {
     return (
-        <div>
-            <div className='container-fluid mt-3'>
-            <div className='container card'>
+        <div className='contain ' >
+        
+            <div className='container-fluid mt-3  '>
+            <div className='adjust'>
+            <div className='container containers card decorate'>
             <div className='row' >
-            <h3 className='text-danger center col-11'> Login Credential</h3>
-            <h5 className='col-1'><Link to='/Register'>Register</Link></h5>
+            <h3 className='text-danger  col-lg-8 mp-3 fst-italic animate__animated animate__tada ' > Login Credential :~ </h3>
+            <h3 className='col-lg'></h3>
+           <button type="button" className='col-lg-2  button text-wrap'  ><Link style={{color: "black"}} to='/Register'>Register</Link></button>
             </div><br/>
             
             <form onSubmit={props.handle}>
             <div className='form-group'>
-            <label htmlFor='emailInput'>Email :-&emsp;</label>
+            <label htmlFor='emailInput' className='text-dark font-weight-bold fs-5 fw-bolder'>Email :-&emsp;</label>
             <input className='form-control' type='email' name='email' id='emailInput'/> 
             </div><br/>
              <div className='form-group'>
-            <label className='text-success' htmlFor='passInput'>Password:- &emsp;</label>
+            <label className='text-dark fw-bolder fs-5' htmlFor='passInput'>Password:- &emsp;</label>
             <input className='form-control' type='password' name='pass' id='passInput'/> 
-            </div><br/>
-            <button type='submit ' className='text-black btn btn-danger'>Login</button>&emsp;
-            <input type='reset' className='text-danger btn btn-black'/>      
+            </div><br/><br/>
+            <button type='submit ' className='text-black btn btn-danger button'>Login</button>&emsp;
+            <input type='reset' className='text-danger btn btn-light'/>      
             
             </form>
+            </div>
             </div>
             </div>
         </div>
--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { cloneElement } from 'react';
 import './Register.css';
 import { BrowserRouter as Router, Switch , Link ,Route } from 'react-router-dom';
 
@@ -13,44 +13,44 @@ const Register = (props) => {
     return (
         
             <div className='container-fluid mt-3'>
-            <div className='container card'>
+            <div className='container card decorate'>
             <div className='row' >
-            <h3 className='text-danger center col-11'> Registeration Form</h3>
-            <h5 className='col-1'><Link to='/Login'>Login</Link></h5>
+            <h3 className='text-danger  col-11 font-weight-bold fs-2'> Registeration Form : ~ </h3>
+            <button   className='col-1 btn-dark btn-text-primary button'><Link style={{color: "black"}} to='/Login'>Login</Link></button>
             </div><br/>
             <p className='text-center'>{props.message}</p>
-            <button className='btn btn-danger border border-black' onClick={props.google}>Sign In With Google 🥸</button><br/>
-            <button className='btn btn-primary border border-primary' onClick={props.fb}>Sign In With Facebook 🫠</button><br/>  
+            <button className='btn btn-danger border border-black m-auto' onClick={props.google} style={{width : '50%'}}>Sign In With Google 🥸</button><br/>
+            <button className='btn btn-primary border border-primary m-auto' onClick={props.fb} style={{width : '50%'}}>Sign In With Facebook 🫠</button><br/>  
             <form onSubmit = {props.register}>
             <div className='form-group'>
-            <label style={style} htmlFor='emailInput'>Email :-&emsp;</label>
+            <label style={style} htmlFor='emailInput' className='text-dark font-weight-bold fs-5 fw-bolder'>Email :-&emsp;</label>
             <input className='form-control' type='email' name='email' id='emailInput'/> 
             </div><br/>
             <div className='row'>
-            <div className='form-group col'>
-            <label className='text-primary' htmlFor='firstNameInput'> First Name:- &emsp;</label>
+            <div className='form-group col' style={{borderRadius : '15px 50px'}}>
+            <label className='text-primary text-dark font-weight-bold fs-5 fw-bolder' htmlFor='firstNameInput' > First Name:- &emsp;</label>
             <input className='form-control' type='text' name='firstName' id='firstNameInput'/> 
             </div>
             <div className='form-group col'>
-            <label className='text-dander' htmlFor='lastNameInput'> Last Name:- &emsp;</label>
+            <label className='text-dander text-dark font-weight-bold fs-5 fw-bolder' htmlFor='lastNameInput'> Last Name:- &emsp;</label>
             <input className='form-control' type='text' name='lastName' id='lastNameInput'/> 
             </div>
-            </div><br/>
+            </div>
             
              <div className='form-group'>
-            <label className='text-success' htmlFor='passInput'>Password:- &emsp;</label>
+            <label className='text-success text-dark font-weight-bold fs-5 fw-bolder' htmlFor='passInput'>Password:- &emsp;</label>
             <input className='form-control' type='password' name='pass' id='passInput'/> 
             </div><br/>
              <div className='form-group'>
-            <label className='text-danger' htmlFor='passInput1'> Confirm Password:- &emsp;</label>
+            <label className='text-danger text-dark font-weight-bold fs-5 fw-bolder' htmlFor='passInput1'> Confirm Password:- &emsp;</label>
             <input className='form-control' type='password' name='confirm' id='passInput1'/> 
             </div><br/>
             <div className='form-group'>
-            <label className='text-warning' htmlFor='phoneInput'>Phone :- &emsp;</label>
+            <label className='text-warning text-dark font-weight-bold fs-5 fw-bolder' htmlFor='phoneInput'>Phone :- &emsp;</label>
             <input className='form-control' type='text' name='phone' id='phoneInput'/> 
             </div><br/>
             <button type='submit '  className='text-black btn btn-danger'>Register</button>&emsp;
-            <input type='reset' className='text-danger btn btn-black'/>    
+            <input type='reset' className='text-danger btn btn-light'/>    
             {props.message && <div>{props.message}</div>}  
             
             </form>
