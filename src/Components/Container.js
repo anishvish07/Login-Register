03d97af20b27854a90be36fe22385f7103be5c66
anishvish07 <dumbodare12@gmail.com>
2023-10-30 21:48:30 +0530

    first commit

--- src/Components/Container.js
+++ src/Components/Container.js
@@ -2,22 +2,24 @@ import React, { Component } from 'react';
 import Register from './Register';
 import Login from './Login';
 import Modal from './modal';
+import PopUp from './Popup';
 import './modal.css';
 import { v4 as uuidv4 } from 'uuid';
 import { initializeApp } from 'firebase/app';
 import { getDatabase, ref, set } from "firebase/database";
-import { BrowserRouter as Router ,Routes, Link ,Navigate ,Route } from 'react-router-dom';
+import { BrowserRouter as Router ,Routes,Navigate ,Route } from 'react-router-dom';
 import { getAuth, createUserWithEmailAndPassword,signInWithEmailAndPassword ,GoogleAuthProvider ,FacebookAuthProvider, signInWithPopup} from 'firebase/auth';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyALIRloAhL_EchxRgcscub2NY4mhUDsZUI",
-  authDomain: "anish-form.firebaseapp.com",
-  projectId: "anish-form",
-  storageBucket: "anish-form.appspot.com",
-  messagingSenderId: "366738935742",
-  appId: "1:366738935742:web:e5a3b66fc8c3f06179f3ef",
-  measurementId: "G-8E7TZQ5JNM"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL : process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
@@ -33,12 +35,18 @@ class Container extends Component {
             password: null,
             isSubmitted: false,
             firstName: null,
+            isPopUpOpen: false,
             lastName: null,
             phone: null,
             confirmpassword :null,
         }
     }
-    
+    openPop = ()=>{
+        this.setState({isPopUpOpen : true});
+    }
+    closePop =()=>{
+        this.setState ({isPopUpOpen : false})
+    }
     openModal = () => {
         this.setState({ isModalOpen: true });
     }
@@ -67,15 +75,6 @@ class Container extends Component {
             isSubmitted: true
         },()=>{
             this.openModal();
-            const userData = {
-            email,
-            password,
-            firstName,
-            lastName,
-            confirmpassword,
-            phone
-        };
-        localStorage.setItem('userData', JSON.stringify(userData));
 
         console.log(email, password, confirmpassword, firstName, lastName,  phone);
                            const dbRef = ref(database, 'form/' + this.state.id);
@@ -119,6 +118,9 @@ class Container extends Component {
             console.error('Error registering user:', error);
           }); 
     }
+    loginHandler2 = ()=>{
+    
+    }
     loginHandler = (events)=>{
         events.preventDefault();
         const email = events.target.email.value;
@@ -126,6 +128,7 @@ class Container extends Component {
         signInWithEmailAndPassword(auth,email,password).then((userCredential)=>{
             const user = userCredential.user;
             alert('Login Successfully');
+               this.openPop();
         }).catch((err)=>{
             alert('Login error' , err);
             alert('Invalid credentials. Please try again.');
@@ -161,31 +164,17 @@ class Container extends Component {
     }
 
 
-    submitHandler2 = (handle)=>{
-        handle.preventDefault();
-        alert('Logged In');
-        const storedUserData = JSON.parse(localStorage.getItem('userData'));
-        // Inside your login function
-        const userEmail = handle.target.email.value;
-        const userPassword = handle.target.pass.value;
 
-    if (userEmail === storedUserData.email && userPassword === storedUserData.password) {
-    alert('Login successful!');
-} else {
-    alert('Invalid credentials. Please try again.');
-}
-//   {email === null && password === null && !isSubmitted ? }
-
-    }
+    
     
    render() {
-        const {  isModalOpen, email,password, lastName, firstName, phone, confirmpassword ,isSubmitted } = this.state;
+        const {  isModalOpen, isPopUpOpen, email,password, lastName, firstName, phone, confirmpassword ,isSubmitted } = this.state;
         return(
       <Router>
             <div>
                 <Routes>
                     <Route path='/Register' element={<Register click={this.submitHandler} fb={this.facebookHandler} register={this.registrationHandler} google={this.googleLoginHandler} message={this.state.message} />} />
-                    <Route path='/Login' element={<Login handle={this.loginHandler} />} />
+                    <Route path='/Login' element={<Login handle={this.loginHandler} />  }  />
                     <Route
                         path='/'
                         element={
@@ -205,7 +194,17 @@ class Container extends Component {
                             <p>Your form has been successfully submitted.</p>
                         </>
                     )}
+
                 </Modal>
+               <PopUp isOpen={isPopUpOpen} onClose={this.closePop}>
+                  {isPopUpOpen && (
+                   <>
+            <h2>Login Successfully!</h2>
+            <p>Your form has been successfully logged In.</p>
+                 </>
+                )}
+              </PopUp>
+                
         </Router>
         )
        
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -12,7 +12,7 @@ const Login = (props) => {
             <div className='container con small-card card decorate'>
             <div >
             <div className='colmn'>
-            <h3 className=' mp-3 fst-italic animate__animated animate__tada ' > Login Credential :~ </h3>
+            <h3 className=' text-white mp-3 fst-italic animate__animated animate__tada ' > Login Credential :~ </h3>
            </div>
            <div className='colmn1'>
            <button type="button" className=' button text-wrap'  ><Link style={{color: "black"}} to='/Register'>Register</Link></button>
@@ -20,11 +20,11 @@ const Login = (props) => {
             
             <form onSubmit={props.handle}>
             <div className='form-group'>
-            <label htmlFor='emailInput' className='text-dark font-weight-bold fs-5 fw-bolder'>Email :-&emsp;</label>
+            <label htmlFor='emailInput' className='text-white font-weight-bold fs-5 fw-bolder'>Email :-&emsp;</label>
             <input className='form-control' type='email' name='email' id='emailInput'/> 
             </div>
              <div className='form-group'>
-            <label className='text-dark fw-bolder fs-5' htmlFor='passInput'>Password:- &emsp;</label>
+            <label className='text-white fw-bolder fs-5' htmlFor='passInput'>Password:- &emsp;</label>
             <input className='form-control' type='password' name='pass' id='passInput'/> 
             </div><br/><br/>
             <button type='submit ' className='text-black btn btn-danger button'>Login</button>&emsp;
