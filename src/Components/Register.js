6b15bbc305fb3f7ddb6d9c3972f2fd8dbf308c2d
anishvish07 <dumbodare12@gmail.com>
2023-11-01 11:56:01 +0530

    first commit

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -27,7 +27,7 @@ const Register = (props) => {
             
            </div>
             <div className='decorate2'>
-             <p className=' font-weight-bold fs-1 animate__animated animate__tada txt colmn'> Registeration Form : ~ </p>
+             <p className=' font-weight-bold fs-1 animate__animated animate__tada  txt colmn'> Registeration Form : ~ </p>
              <button   className=' colmn1 button text-wrap'><Link style={{color: "black"}} to='/Login'>Login</Link></button>
            </div>
             <p className='text-center text-danger animate__animated animate__fadeIn'>{props.message}</p>
@@ -35,32 +35,32 @@ const Register = (props) => {
             <button className='btn btn-primary border border-primary  m-auto' onClick={props.fb} style={{width : '50%'}}>Sign In With Facebook <span class="fb-like-emoji">F</span></button><br/>  
             <form onSubmit = {props.register}>
             <div className='form-group'>
-            <label style={style} htmlFor='emailInput' className='text-dark font-weight-bold fs-5 fw-bolder'>Email :-&emsp;</label>
+            <label style={style} htmlFor='emailInput' className='text-light font-weight-bold fs-5 fw-bolder'>Email :-&emsp;</label>
             <input className='form-control' type='email' name='email' id='emailInput'  placeholder='Ex-anish@gmail.com ' required/> 
             </div>
             <div className='row'>
             <div className='form-group col' style={{borderRadius : '15px 50px'}}>
-            <label className='text-primary text-dark font-weight-bold fs-5 fw-bolder' htmlFor='firstNameInput' > First Name:- &emsp;</label>
+            <label className='text-primary text-light font-weight-bold fs-5 fw-bolder' htmlFor='firstNameInput' > First Name:- &emsp;</label>
             <input className='form-control' type='text' name='firstName' id='firstNameInput'  required/> 
             </div>
             <div className='form-group col'>
-            <label className='last font-weight-bold fs-5 fw-bolder' htmlFor='lastNameInput'> Last Name:- &emsp;</label>
-            <input className='form-control' type='text' name='lastName' id='lastNameInput' required/> 
+            <label className='last text-info font-weight-bold fs-5 fw-bolder' htmlFor='lastNameInput'> Last Name:- &emsp;</label>
+            <input className='form-control' type='text' name='lastName' id='lastNameInput' placeholder='Last name' required/> 
             </div>
             </div>
             
              <div className='form-group'>
-            <label className='text-success text-dark font-weight-bold fs-5 fw-bolder' htmlFor='passInput'>Password:- &emsp;</label>
+            <label className=' text-light font-weight-bold fs-5 fw-bolder' htmlFor='passInput'>Password:- &emsp;</label>
             <input className='form-control' type='password' name='pass' id='passInput'  required  placeholder='Ex-Abcd123$'/> 
             </div>
              <div className='form-group'>
-            <label className='text-danger text-dark font-weight-bold fs-5 fw-bolder' htmlFor='passInput1'> Confirm Password:- &emsp;</label>
+            <label className=' text-light font-weight-bold fs-5 fw-bolder' htmlFor='passInput1'> Confirm Password:- &emsp;</label>
             <input className='form-control' type='password' name='confirm' id='passInput1' required/> 
             </div>
             <div className='form-group'>
-            <label className='text-warning text-dark font-weight-bold fs-5 fw-bolder' htmlFor='phoneInput'>Phone :- &emsp;</label>
+            <label className=' text-light font-weight-bold fs-5 fw-bolder' htmlFor='phoneInput'>Phone :- &emsp;</label>
             <input className='form-control' type='text' name='phone' id='phoneInput'/> 
-            </div><br/><div></div>
+            </div><br/>
             <button type='submit ' className='text-dark btn-lg btn-sm btn-danger fs-5 '>Register</button>&emsp;
             <input type='reset' className='text-danger btn-lg btn-sm btn-dark fs-6'/>    
             <h5 style={{
